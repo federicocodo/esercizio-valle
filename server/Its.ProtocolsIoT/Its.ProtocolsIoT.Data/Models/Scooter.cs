3514191c3ea38e1292bfb0b911b52485ffb16e2a
federicocodo <fede.codo38@gmail.com>
2020-12-17 12:14:03 +0100

    Commit Web Application server

---
+++ server/Its.ProtocolsIoT/Its.ProtocolsIoT.Data/Models/Scooter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Its.ProtocolsIoT.Data.Models
+{
+    public class Scooter
+    {
+        public double Speed { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public Scooter() { }
+
+        public Scooter(double speed, double latitude, double longitude)
+        {
+            Speed = speed;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
---
+++ server/Its.ProtocolsIoT/Its.ProtocolsIoT.Data/Models/ScooterTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Its.ProtocolsIoT.Data.Models
+{
+    public class ScooterTable : TableEntity
+    {
+        public double Speed { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public ScooterTable() { }
+
+        public ScooterTable(double speed, double latitude, double longitude)
+        {
+            Speed = speed;
+            Latitude = latitude;
+            Longitude = longitude;
+
+            PartitionKey = "Its";
+            RowKey = Guid.NewGuid().ToString();
+        }
+
+        //id dispositivo partition key
+        //guid row key
+    }
+}

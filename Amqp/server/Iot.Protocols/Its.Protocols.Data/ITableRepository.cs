eda4740fab5b32c457d809ae7181e9448bb19ad3
federicocodo <fede.codo38@gmail.com>
2021-01-21 12:47:47 +0100

    commit amqt server

---
+++ Amqp/server/Iot.Protocols/Its.Protocols.Data/ITableRepository.cs
@@ -0,0 +1,13 @@
+﻿using Its.Protocols.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Its.Protocols.Data
+{
+    public interface ITableRepository
+    {
+        Task Insert(string deviceId, Scooter scooter);
+    }
+}
---
+++ Amqp/server/Iot.Protocols/Its.Protocols.Data/Models/Scooter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Its.Protocols.Data.Models
+{
+    public class Scooter
+    {
+        public double Speed { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
